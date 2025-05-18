7c9ae8b3d2a7cf3c719a1ecc837f0a49b40750e0
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-05-18 17:56:41 +0545

    setup auth and modified the prisma configs for integrating with better auth

--- libs/auth/src/lib/auth.ts
+++ libs/auth/src/lib/auth.ts
@@ -1,3 +1,15 @@
-export function auth(): string {
-  return 'auth';
-}
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "database";
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:1234'
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [FRONTEND_URL],
+});
--- libs/database/src/index.ts
+++ libs/database/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./generated"
+export * from "../prisma/prisma-db"
