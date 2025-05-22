61858bad3f23810199fdf5b6836a5c1e1843437d
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-05-22 23:11:22 +0545

    better auth setup completed

---
+++ libs/auth/src/lib/auth-client.ts
@@ -0,0 +1,9 @@
+import { adminClient } from 'better-auth/client/plugins';
+import { createAuthClient } from 'better-auth/react';
+
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:4321';
+
+export const authClient = createAuthClient({
+  baseURL: BACKEND_URL,
+  plugins: [adminClient()],
+});
---
+++ libs/auth/src/lib/auth-server.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { admin } from 'better-auth/plugins';
+import { db } from '@recipedia/database';
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:1234';
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [FRONTEND_URL],
+  plugins: [admin()],
+});
